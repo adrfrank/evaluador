138ae61b68a69e55f0a6dee2b97ea7c8a3312b68
Adrian Francisco Gonzalez Gutierrez <guanoman_adrian@hotmail.com>
2013-09-27 08:09:21 -0500

    Agregada la pagina inicial

---
+++ OmijalWebsite/Index.php
@@ -0,0 +1,26 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>Omijal</title>
+    </head>
+    <body>
+        <header>
+            <section>
+            </section>
+            <h1>
+                Omijal
+            </h1>
+        </header>
+        <section id="main-container">
+            <section id="slider">
+            </section>
+            <section id="sub-container">
+            </section>
+        </section>        
+    </body>
+</html>
